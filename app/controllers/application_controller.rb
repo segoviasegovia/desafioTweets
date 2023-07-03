478c9e7f6affde7ca3524e5e7dfa4462dc8c4e1a
Victor Segovia <segovia_victor@hotmail.com>
2023-07-03 03:18:14 +0000

    version final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    Pagy::DEFAULT[:items] = 10
+    Pagy::DEFAULT[:items] = 5
     include Pagy::Backend
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,7 @@ class TweetsController < ApplicationController
   def index
     @pagy, @tweets = pagy(Tweet.all)
     if params[:query_text].present?
+      #@pagy, @tweets = pagy(@tweets.search_full_text(params[:query_text]))
       @Tweets=@tweets.search_full_text(params[:query_text])
     end
   end
